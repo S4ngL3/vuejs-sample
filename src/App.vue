6dc0f4884cac03cbd12f6c7479a6013c17245a38
LeAnhSang <sangla@f88.vn>
2022-03-26 21:57:41 +0700

    Update codes

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <FingerPrint msg="Unique device information"/>
+</template>
+
+<script>
+import FingerPrint from './components/finger-print.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FingerPrint
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/finger-print.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+    <ul>
+      <li v-for="d in fingerPrint()" :key="d.uuid">{{ d }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { DeviceUUID } from "device-uuid";
+export default {
+  data() {
+    return {
+      uuid: new DeviceUUID().get(),
+      du: new DeviceUUID().parse(),
+    };
+  },
+  name: "Finger-Print",
+  props: {
+    msg: String,
+  },
+  methods: {
+    fingerPrint() {
+      let du = new DeviceUUID().parse();
+      let dua = [
+        du.language,
+        du.platform,
+        du.os,
+        du.cpuCores,
+        du.isAuthoritative,
+        du.silkAccelerated,
+        du.isKindleFire,
+        du.isDesktop,
+        du.isMobile,
+        du.isTablet,
+        du.isWindows,
+        du.isLinux,
+        du.isLinux64,
+        du.isMac,
+        du.isiPad,
+        du.isiPhone,
+        du.isiPod,
+        du.isSmartTV,
+        du.pixelDepth,
+        du.isTouchScreen,
+      ];
+      var uuid = new DeviceUUID().get();
+      var uuid2 = du.hashMD5(dua.join(":"));
+      var uuid3 = du.hashInt(dua.join(":"));
+
+      return [
+        "Device UUID: " + uuid,
+        "Fingerprint-2: " + uuid2,
+        "Fingerprint-3: " + uuid3,
+      ];
+    },
+  },
+};
+</script>
